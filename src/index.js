2af4e3ac5d40e33d94215c162ea0e3e6386c8d60
Dakota <dakotakcoppage@me.com>
2021-07-20 07:30:03 -0600

    created initial store in index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
-const user = () => []
+const users = () => []
 const reducer = combineReducers({
   users
 })
