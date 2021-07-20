971ed927520f882b98817b6b4a7dae94ad88bee5
Dakota <dakotakcoppage@me.com>
2021-07-20 07:58:52 -0600

    created store.js and imported into index.js to clean up code

--- src/index.js
+++ src/index.js
@@ -3,17 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import userReducers from './reducers/users.js'
+import store from './store.js'
 
-
-const reducer = combineReducers({
-  usersReducer
-})
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,6 +1,8 @@
-export default { state = [], action } => {
+const usersReducers = ( state = [], action ) => {
     switch (action.type) {
         default:
             return state;
     }
 }
+
+export default usersReducers
