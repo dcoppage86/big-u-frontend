b26641706adf2cd84d603927b521db404ab2e78f
Dakota <dakotakcoppage@me.com>
2021-07-20 16:01:17 -0600

    created login form setup in Login component

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <form onSubmit={}>
+            <input value={} name="username" type="text" onChange={} />
+            <input value={} name="password" type="text" onChange={} />
+            <input type="submit" value="login" />
+        </form>
+    )
+}
