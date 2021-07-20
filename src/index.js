a051832de57f4660f0c76632421940cf673e059c
Dakota <dakotakcoppage@me.com>
2021-07-20 07:24:13 -0600

    creating initial store with mock user

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+
+const user = () => []
+const reducer = combineReducers({
+  users
+})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={ store }>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
