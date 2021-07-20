c8550352f7e350996f126645bf9d32189d5252d7
Dakota <dakotakcoppage@me.com>
2021-07-20 15:41:14 -0600

    added fetch request POST for currentUser

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import usersReducers from './reducers/users.js'
+import currentUser from './reducers/currentUser.js'
 
 const reducer = combineReducers({
-  users: usersReducers
+  users: usersReducers,
+  currentUser
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
