658b67a6210d8394cd3851b1c2cc7a7aa15bb77f
Dakota <dakotakcoppage@me.com>
2021-07-21 09:07:54 -0600

    created initial main container structure

---
+++ src/components/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainContainer = () => {
+    return (
+        <div className="MainContainer">
+            This is the main container
+        </div>
+    )}
+
+
+export default MainContainer;
