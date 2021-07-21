6ea88cc8a9270d6924bf3d404f7931882385e22e
Dakota <dakotakcoppage@me.com>
2021-07-21 09:08:57 -0600

    created nav bar that will populate for future routes upon checking for a current user login

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import Login from './Login';
+import Logout from './Logout';
+
+const NavBar = ({ currentUser }) => {
+    return (
+        <div className="NavBar">
+            {currentUser ? <strong>Welcome, {currentUser.first_name}</strong> : ""}
+            {currentUser ? <Logout/> : <Login/>}
+        </div>
+    )
+}
+
+const mapStateToProps = ({ currentUser }) => {
+    return {
+      currentUser
+    }
+  }
+
+export default connect(mapStateToProps)(NavBar);
