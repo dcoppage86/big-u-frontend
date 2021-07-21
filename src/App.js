723e5edbb4dc5a7a3fbd1c46f409976e5410431c
Dakota <dakotakcoppage@me.com>
2021-07-21 09:07:21 -0600

    created app structure with navbar and main container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Login from "./components/Login.js"
-import Logout from './components/Logout';
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser.js"
+import NavBar from "./components/NavBar.js"
+import MainContainer from "./components/MainContainer.js"
 
 class App extends React.Component {
 
@@ -13,15 +13,14 @@ class App extends React.Component {
   
   render() {
     return (
-      this.props.currentUser ? <Logout /> : <Login />
+      <div className="app">
+        <NavBar />
+        <MainContainer />
+      </div>
+
     );
 
   }
 }
 
-const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser
-  }
-}
-export default connect(mapStateToProps, { getCurrentUser })(App);
+export default connect(null, { getCurrentUser })(App);
