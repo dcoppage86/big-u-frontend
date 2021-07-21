640ed3b23210d531a0ac6936f0b155b666191386
Dakota <dakotakcoppage@me.com>
2021-07-20 22:55:05 -0600

    finishing currentUser.js and Login.js

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -9,13 +9,23 @@ export const setCurrentUser = user => {
 
 // asynchronous 
 export const login = credentials => {
+    console.log("credentials are", credentials)
     return dispatch => {
-        return fetch("http://localhost:3001/api/v1/login"), {
+        return fetch("http://localhost:3001/api/v1/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({username: "TTest01", password: "password"})
-        }
+            body: JSON.stringify(credentials)
+        })
+        .then(r => r.json())
+        .then(user => {
+            if (user.error) {
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
     }
 }
