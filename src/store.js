10224cf2208c4619bb081a833a2904783a621940
Dakota <dakotakcoppage@me.com>
2021-07-22 09:14:34 -0600

    added reducer to store

--- src/store.js
+++ src/store.js
@@ -3,11 +3,13 @@ import thunk from 'redux-thunk'
 import usersReducers from './reducers/users.js'
 import currentUser from './reducers/currentUser.js'
 import loginForm from './reducers/loginForm.js'
+import userEntries from './reducers/userEntries.js'
 
 const reducer = combineReducers({
   users: usersReducers,
   currentUser,
-  loginForm
+  loginForm,
+  userEntries
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
