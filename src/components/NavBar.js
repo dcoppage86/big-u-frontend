3d71e443f23091c21ccffa96ab796c0fcb395548
Dakota <dakotakcoppage@me.com>
2021-07-24 12:49:14 -0600

    added navbar bootstrap element and styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,33 @@ import React from 'react';
 import { connect } from 'react-redux'
 import Login from './Login';
 import Logout from './Logout';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
 
-const NavBar = ({ currentUser }) => {
+const NavigationBar = ({ currentUser }) => {
     return (
-        <div className="NavBar">
-            {currentUser ? <strong>Welcome, {currentUser.attributes.first_name}</strong> : ""}
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand href="/">BIG-U</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#features">About</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Entries</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Library</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">More deets</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
             {currentUser ? <Logout/> : <Login/>}
-        </div>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+        
     )
 }
 
@@ -18,4 +38,4 @@ const mapStateToProps = ({ currentUser }) => {
     }
   }
 
-export default connect(mapStateToProps)(NavBar);
+export default connect(mapStateToProps)(NavigationBar);
