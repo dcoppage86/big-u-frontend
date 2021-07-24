598f5ff3c4e9c7398b5b49e3f9d5d20fced41733
Dakota <dakotakcoppage@me.com>
2021-07-24 09:15:59 -0600

    importing user entries component

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,10 @@
 import React from 'react'
-// import UserEntries from './UserEntries'
+import UserEntries from './UserEntries'
 
 const MainContainer = () => {
     return (
         <div className="MainContainer">
-            {/* <UserEntries /> */}
+            <UserEntries />
         </div>
     )}
 
