91589ba2e40e490db2bd824406a46eecbf120e3b
Dakota <dakotakcoppage@me.com>
2021-07-24 09:17:16 -0600

    created component, connected for entry state and mapped over the entries to produce the card component

--- src/components/UserEntries.js
+++ src/components/UserEntries.js
@@ -1,15 +1,18 @@
-// import React from 'react'
-// import { connect } from 'react-redux'
+import React from 'react'
+import EntryCard from './EntryCard'
+import { connect } from 'react-redux'
 
-// const UserEntries = () => {
-//     return (
+const UserEntries = props => {
+    const entryCards = props.entries.map(ec => <EntryCard daily_entry={ec} key={ec.id}/>)
+    return (
+        entryCards.length > 0 ? entryCards : null
+    )
+}
 
-//     )
-// }
-
-// const mapStateToProps = state => {
-//     return {
-//         entries: state.
-//     }
-// }
-// export default UserEntries
+//  mapStateToProps to redux to get redux to provide the pieces of state to the component as props
+const mapStateToProps = state => {
+    return {
+        entries: state.userEntries
+    }
+}
+export default connect(mapStateToProps)(UserEntries)
