d18040c42741db6e608321aa95e886386278dc62
Dakota <dakotakcoppage@me.com>
2021-07-25 00:22:32 -0600

    added list items to home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-            <div className="home-container">
-                <h1>Welcome to Big-U</h1>
-                <h3>A place to vent, discuss and educate</h3>
-                <br></br>
-                <br></br>
-                <strong>What is Autism?</strong>
+        <div className="home-container">
+            <h1>Welcome to Big-U</h1>
+            <h3>A place to vent, discuss and educate</h3>
+            <br></br>
+            <br></br>
+            <strong>What is Autism?</strong>
                 <ul>
                     <li>Autism is a bio-neurological developmental disability that generally appears before the age of 3</li>
                     <li>Autism impacts the normal development of the brain in the areas of social interaction, communication skills, and cognitive function. Individuals with autism typically have difficulties in verbal and non-verbal communication, social interactions, and leisure or play activities</li>
@@ -16,7 +16,21 @@ const HomePage = () => {
                     <li>Autism itself does not affect life expectancy, however research has shown that the mortality risk among individuals with autism is twice as high as the general population, in large part due to drowning and other accidents.</li>
                     <li>Currently there is no cure for autism, though with early intervention and treatment, the diverse symptoms related to autism can be greatly improved and in some cases completely overcome.</li>
                 </ul>
-            </div>
+            <br></br>
+            <br></br>
+            <strong>Autism Facts & Stats</strong>
+                <ul>
+                    <li>Autism now affects 1 in 54 children; over half are classified as having an intellectual disability or borderline intellectual disability</li>
+                    <li>Boys are four times more likely to have autism than girls</li>
+                    <li>About 40% of children with autism do not speak. About 25%–30% of children with autism have some words at 12 to 18 months of age and then lose them. Others might speak, but not until later in childhood</li>
+                    <li>Autism greatly varies from person to person (no two people with autism are alike)</li>
+                    <li>The rate of autism has steadily grown over the last twenty years</li>
+                    <li>Autism is the fastest growing developmental disorder, yet most underfunded</li>
+                    <li>2008 Danish Study found that the mortality risk among those with autism was nearly twice that of the general population</li>
+                    <li>Children with autism do progress – early intervention is key</li>
+                    <li>Autism is treatable, not a hopeless condition</li>
+                </ul>
+        </div>
     )
 }
 
