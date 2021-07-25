1c78fb1bf7163782fef9586b99c650c4023d9b42
Dakota <dakotakcoppage@me.com>
2021-07-25 00:26:30 -0600

    created signupForm reducer and added initialState

---
+++ src/reducers/signupForm.js
@@ -0,0 +1,17 @@
+const initialState = {
+    first_name: "",
+    last_name: "",
+    username: "",
+    email: "",
+    password: "",
+    collection_id: ""
+}
+
+const signupForm = (state=initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default signupForm
