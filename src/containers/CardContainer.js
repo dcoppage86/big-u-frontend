1a6f372e0356d051100b92ece9b0f29a5142935d
Dakota <dakotakcoppage@me.com>
2021-07-26 10:10:46 -0600

    added padding

--- src/containers/CardContainer.js
+++ src/containers/CardContainer.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import UserEntries from '../components/UserEntries'
 import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
 
+const StyledContainer = styled(Container)`
+    padding-top: 25px
+`
 
 const CardContainer = (props) => {
     return (
-        <Container>
+        <StyledContainer>
             <div className="Card-Container">
             <h3>Daily Entries</h3>
             <p>Keep track of your daily struggles and victories, no matter how big or small</p>
             <br></br>
             <UserEntries/>
             </div>
-        </Container>
+        </StyledContainer>
     )}
 
 
