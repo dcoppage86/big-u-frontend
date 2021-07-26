dc7a7c2040b5c3535ac16ad8ad96f7feee27a403
Dakota <dakotakcoppage@me.com>
2021-07-25 20:51:34 -0600

    created signup form and working on the submit functionality

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { updateSignupForm } from '../actions/SignUp';
-import { signup } from '../actions/currentUser';
+import { updateSignupForm } from '../actions/signupForm';
+// import { signup } from '../actions/currentUser';
 
 
-const Signup = ({ signupFormData, updatesignupForm, signup }) => {
+const Signup = ({ signupFormData, updateSignupForm }) => {
     const handleInputChange = event => {
         const { name, value } = event.target
         const updatedFormInfo = {
@@ -16,11 +16,15 @@ const Signup = ({ signupFormData, updatesignupForm, signup }) => {
 
     const handleSubmit = event => {
         event.preventDefault()
-        signup(signupFormData)
+        // signup(signupFormData)
     }
 
     return (
         <form onSubmit={ handleSubmit }>
+            <input placeholder="first_name" value={signupFormData.first_name} name="first_name" type="text" onChange={ handleInputChange } />
+            <input placeholder="last_name" value={signupFormData.last_name} name="last_name" type="text" onChange={ handleInputChange } />
+            <input placeholder="childs_name" value={signupFormData.childs_name} name="childs_name" type="text" onChange={ handleInputChange } />
+            <input placeholder="email" value={signupFormData.email} name="email" type="text" onChange={ handleInputChange } />
             <input placeholder="username" value={signupFormData.username} name="username" type="text" onChange={ handleInputChange } />
             <input placeholder="password" value={signupFormData.password} name="password" type="text" onChange={ handleInputChange } />
             <input type="submit" value="signup" />
@@ -32,4 +36,4 @@ const mapStateToProps = state => {
         signupFormData: state.signupForm
     }
 }
-export default connect(mapStateToProps, { updatesignupForm, signup })(signup);
+export default connect(mapStateToProps, { updateSignupForm})(Signup);
