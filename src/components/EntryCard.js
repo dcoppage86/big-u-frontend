04be5c9cc12131f524a213b9b427ef5f1c95db4c
Dakota <dakotakcoppage@me.com>
2021-07-26 09:00:27 -0600

    added styled component for card styling in EntryCard

--- src/components/EntryCard.js
+++ src/components/EntryCard.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import styled from 'styled-components'
+import { Container } from 'react-bootstrap'
+
+const StyledContainer = styled(Container)`
+    display: flex;
+    flex-flow: column wrap;
+    text-align: center;
+    justify-content: space-evenly
+    
+`
 
 const EntryCard = ({ daily_entry }) => {
     return (
-        <Card style={{ width: '18rem' }}>
+        <StyledContainer>
+        <Card>
             <Card.Body>
                 <Card.Title>{daily_entry.attributes.title}</Card.Title>
                 <Card.Text>
@@ -13,6 +24,7 @@ const EntryCard = ({ daily_entry }) => {
                 <Card.Link href="#">Edit</Card.Link>
             </Card.Body>
         </Card>
+        </StyledContainer>
 
 
     )
