fae4d293ed3226198da3c8619865f04e883b60e0
Dakota <dakotakcoppage@me.com>
2021-07-26 15:54:04 -0600

    correcting: this is where updated initialState

--- src/reducers/signupForm.js
+++ src/reducers/signupForm.js
@@ -1,14 +1,18 @@
 const initialState = {
     first_name: "",
     last_name: "",
+    childs_name: "",
     username: "",
     email: "",
-    password: "",
-    collection_id: ""
+    password: ""
 }
 
 const signupForm = (state=initialState, action) => {
     switch (action.type) {
+        case "UPDATE_SIGNUP_FORM":
+            return action.formData
+        case "RESET_SIGNUP_FORM":
+            return initialState
         default:
             return state
     }
