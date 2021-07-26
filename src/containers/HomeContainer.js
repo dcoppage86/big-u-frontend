9a3fce7c39e726c323a290e4e1e7f5c01410bea7
Dakota <dakotakcoppage@me.com>
2021-07-26 10:11:07 -0600

    added padding

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import HomePage from '../components/HomePage';
 import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const StyledContainer = styled(Container)`
+    padding-top: 25px
+`
 
 const HomeContainer = (props) => {
     return (
-        <Container>
+        <StyledContainer>
             <div className="home-page-container">
             <HomePage/>
             </div>
-        </Container>
+        </StyledContainer>
     )}
 
 
