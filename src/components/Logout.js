a6971d96ccf373d6594fe8ff19285fc12938ea0c
Dakota <dakotakcoppage@me.com>
2021-07-26 23:56:03 -0600

    added a handleSubmit function to log out user and redirect to the home page

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser';
+import { useHistory } from 'react-router';
 
 
 const Logout = ({ logout }) => {
+    const history = useHistory()
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        logout()
+        history.push('/')
+    }
 
     return (
-        <form onSubmit={ logout }>
+        <form onSubmit={ handleSubmit }>
             <button className="btn btn-dark btn-lg btn-block" type="submit" value="Log Out">Log Out</button>
         </form>
     )
