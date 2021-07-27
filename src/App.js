aa13bfb17cf11e5a63d8b29e9e050bd19a9e9db1
Dakota <dakotakcoppage@me.com>
2021-07-27 00:18:15 -0600

    created route for contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser.js"
-import NavBar from "./components/NavBar.js"
+import NavBar from "./components/NavBar.js";
 import CardContainer from './containers/CardContainer';
 import HomeContainer from './containers/HomeContainer';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AboutContainer from './containers/AboutContainer';
 import SignupContainer from './containers/SignupContainer';
-import LoginContainer from './containers/LoginContainer'
+import LoginContainer from './containers/LoginContainer';
+import ContactContainer from './containers/ContactContainer.js';
+
 
 
 
@@ -28,6 +30,7 @@ class App extends React.Component {
             <Route exact path="/signup" component={SignupContainer}/>
             <Route exact path="/user-entries" component={CardContainer}/>
             <Route exact path="/about" component={AboutContainer}/>
+            <Route exact path="/contact" component={ContactContainer}/>
             <Route exact path="/login" component={LoginContainer}/>
           </Switch>
         </div>
