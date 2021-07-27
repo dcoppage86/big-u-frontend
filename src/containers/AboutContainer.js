4c9889efa1eaf25457ba751fa0e3e8bdbd6af258
Dakota <dakotakcoppage@me.com>
2021-07-27 00:18:42 -0600

    changed styling

--- src/containers/AboutContainer.js
+++ src/containers/AboutContainer.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const StyledContainer = styled(Container)`
-    padding-top: 25px
+    padding-top: 100px
 `
 
 const AboutContainer = (props) => {
