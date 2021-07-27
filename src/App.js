baa8cd86f8b216c92361a4b29fc34b9e0ad0ba64
Dakota <dakotakcoppage@me.com>
2021-07-26 19:37:14 -0600

    nothing changed just refactored

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { getCurrentUser } from "./actions/currentUser.js"
 import NavBar from "./components/NavBar.js"
 import CardContainer from './containers/CardContainer';
 import HomeContainer from './containers/HomeContainer';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AboutContainer from './containers/AboutContainer';
 import SignupContainer from './containers/SignupContainer';
 import LoginContainer from './containers/LoginContainer'
@@ -15,6 +15,7 @@ class App extends React.Component {
 
   componentDidMount() {
     this.props.getCurrentUser()
+
   }
   
   render() {
@@ -22,11 +23,13 @@ class App extends React.Component {
       <Router>
         <div className="app">
           <NavBar />
+          <Switch>
             <Route exact path="/" component={HomeContainer}/>
             <Route exact path="/signup" component={SignupContainer}/>
             <Route exact path="/user-entries" component={CardContainer}/>
             <Route exact path="/about" component={AboutContainer}/>
             <Route exact path="/login" component={LoginContainer}/>
+          </Switch>
         </div>
       </Router>
 
