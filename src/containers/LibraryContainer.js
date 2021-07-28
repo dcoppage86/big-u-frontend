134da6cfe64799d97c64846866f8888bc09c02d8
Dakota <dakotakcoppage@me.com>
2021-07-28 08:10:07 -0600

    Library container to take in all book cards from Library.js

---
+++ src/containers/LibraryContainer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Library from '../components/Library';
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
+
+const StyledContainer = styled(Container)`
+    padding-top: 25px
+`
+
+const LibraryContainer = ({currentUser}) => {
+    return (
+        <StyledContainer>
+            {currentUser ? <div className="Library-Container">
+            <h3>Library</h3>
+            <p>These are books may help in your journey, Feel free to add more to the list!</p>
+            <br></br>
+            <Library />
+            </div> : "YOU DO NOT HAVE ACCESS!"}
+        </StyledContainer>
+    )}
+
+    const mapStateToProps = ({ currentUser }) => {
+        return {
+          currentUser
+        }
+      }
+
+export default connect(mapStateToProps)(LibraryContainer)
