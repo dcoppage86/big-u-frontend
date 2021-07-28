5f210e5c1083f241e3ac0b0dacac1271b8f956d2
Dakota <dakotakcoppage@me.com>
2021-07-28 09:07:43 -0600

    removed styled components

--- src/components/EntryCard.js
+++ src/components/EntryCard.js
@@ -1,32 +1,18 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-import styled from 'styled-components'
-import { Container } from 'react-bootstrap'
-
-const StyledContainer = styled(Container)`
-    display: flex;
-    flex-flow: column wrap;
-    text-align: center;
-    justify-content: space-evenly
-    
-`
 
 const EntryCard = ({ daily_entry }) => {
     return (
-        <StyledContainer>
-        <Card>
+        <Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Card.Title>{daily_entry.attributes.title}</Card.Title>
                 <Card.Text>
-                {daily_entry.attributes.content}
+                    {daily_entry.attributes.content}
                 </Card.Text>
                 <Card.Link href="#">Delete</Card.Link>
                 <Card.Link href="#">Edit</Card.Link>
             </Card.Body>
         </Card>
-        </StyledContainer>
-
-
     )
 }
 
