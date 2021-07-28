d6f7f8858130982a39dcc5bbdb63d6d99490fd07
Dakota <dakotakcoppage@me.com>
2021-07-28 08:08:23 -0600

    created contact component stateless

---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <>
+            <h3>Thank You For Being A Part of Big-U</h3>
+            <p>If you have any issues at all you may contact us:</p>
+            <br></br>
+            <strong>Email</strong>
+            <p>bigU@gmail.com</p>
+        </>
+    )
+}
+
+export default Contact;
