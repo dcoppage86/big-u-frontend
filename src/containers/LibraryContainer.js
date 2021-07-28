16cca306382b51471618271dc2d436746e101872
Dakota <dakotakcoppage@me.com>
2021-07-28 09:08:36 -0600

    added div for cards

--- src/containers/LibraryContainer.js
+++ src/containers/LibraryContainer.js
@@ -5,17 +5,19 @@ import styled from 'styled-components';
 import { connect } from 'react-redux';
 
 const StyledContainer = styled(Container)`
-    padding-top: 25px
+    padding-top: 25px;
 `
 
 const LibraryContainer = ({currentUser}) => {
     return (
         <StyledContainer>
             {currentUser ? <div className="Library-Container">
-            <h3>Library</h3>
-            <p>These are books may help in your journey, Feel free to add more to the list!</p>
-            <br></br>
-            <Library />
+                <h3>Library</h3>
+                <p>These are books may help in your journey, Feel free to add more to the list!</p>
+                <br></br>
+                    <div style={{display: 'flex', flexDirection: 'row'}} className="Card-Container">
+                        <Library />
+                    </div>
             </div> : "YOU DO NOT HAVE ACCESS!"}
         </StyledContainer>
     )}
