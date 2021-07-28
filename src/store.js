423aa6c455477b20638a3851d0f703f7facb1c70
Dakota <dakotakcoppage@me.com>
2021-07-28 08:05:39 -0600

    added allBooks reducer

--- src/store.js
+++ src/store.js
@@ -5,13 +5,15 @@ import currentUser from './reducers/currentUser.js'
 import loginForm from './reducers/loginForm.js'
 import userEntries from './reducers/userEntries.js'
 import signupForm from './reducers/signupForm'
+import allBooks from './reducers/books.js'
 
 const reducer = combineReducers({
   users: usersReducers,
   currentUser,
   loginForm,
   userEntries,
-  signupForm
+  signupForm,
+  allBooks
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
