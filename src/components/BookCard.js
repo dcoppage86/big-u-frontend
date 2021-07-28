93cdbd519833b8de1574ed2982205814734ea882
Dakota <dakotakcoppage@me.com>
2021-07-28 09:07:02 -0600

    removed styled components

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,29 +1,17 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
-import styled from 'styled-components'
-import { Container } from 'react-bootstrap'
 
-const StyledContainer = styled(Container)`
-    display: flex;
-    flex-flow: column wrap;
-    text-align: center;
-    justify-content: space-evenly
-    
-`
 
 const BookCard = ({ book }) => {
     return (
-        <StyledContainer>
-        <Card>
+        <Card style={{ width: '18rem' }}>
             <Card.Body>
-                <Card.Title>{book.attributes.title}</Card.Title>
-                <Card.Text>
-                </Card.Text>
-                <Card.Link href="#">Delete</Card.Link>
-                <Card.Link href="#">Edit</Card.Link>
+                <Card.Img variant="top" src={book.attributes.image_url}/>
+                <Card.Title><h3>{book.attributes.title}</h3></Card.Title>
+                <Card.Text><h4>{book.attributes.author}</h4></Card.Text>
+                <Card.Link href={book.attributes.book_url}>Find Here</Card.Link>
             </Card.Body>
         </Card>
-        </StyledContainer>
 
 
     )
