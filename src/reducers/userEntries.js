432bc4b7caf803f70b13ff40127194079b725301
Dakota <dakotakcoppage@me.com>
2021-07-30 08:13:46 -0600

    added filter to find the entry id

--- src/reducers/userEntries.js
+++ src/reducers/userEntries.js
@@ -1,7 +1,13 @@
-const userEntries = (state = [], action) => {
+const initialState = []
+
+const userEntries = (state = initialState, action) => {
     switch (action.type) {
         case "SET_USER_ENTRIES":
             return action.entries
+        case "ADD_ENTRY":
+            return state.concat(action.entry)
+        case "DELETE_ENTRY":
+            return state.filter(entry => entry.id === action.entryId ? false : true)
         default:
             return state
     }
