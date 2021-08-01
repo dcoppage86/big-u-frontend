efed229a169cfec7c8bb96b1f4facb0c79ddc081
Dakota <dakotakcoppage@me.com>
2021-08-01 16:23:26 -0600

    adding bootstrap jsx elements and props

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -4,20 +4,49 @@ import { connect } from 'react-redux'
 import { updateBookForm } from '../actions/bookEntryForm'
 import { Button } from 'react-bootstrap'
 
-const BookForm = () => {
+const BookForm = ({formData, updateBookForm}) => {
+
+    const {title, author, image_url, book_url, category} = formData
+
+    const handleInputChange = event => {
+        const {name, value} = event.target
+        updateBookForm(name, value)
+    }
+
     return (
         <Form>
             <Form.Group>
                 <Form.Label>Title</Form.Label>
-                <Form.Control></Form.Control>
+                <Form.Control type="text" name="title" onChange={ handleInputChange } value={title} placeholder="Enter book title..." />
             </Form.Group>
             <Form.Group>
-                <Form.Label></Form.Label>
-                <Form.Control></Form.Control>
+                <Form.Label>Author</Form.Label>
+                <Form.Control/>
             </Form.Group>
+            <Form.Group>
+                <Form.Label>Book Image</Form.Label>
+                <Form.Control/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Book Url</Form.Label>
+                <Form.Control/>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Category</Form.Label>
+                <Form.Select/>
+            </Form.Group>
+            <br></br>
             <Button type="submit" className="btn btn-dark btn-lg btn-block" value="Add Book">Add Book</Button>
         </Form>
     )
 }
 
-export default BookForm
+const mapStateToProps = state => {
+    const userId = state.currentUser ? state.currentUser.id : ""
+    return {
+        formData: state.bookEntryForm,
+        userId
+    }
+}
+
+export default connect(mapStateToProps, { updateBookForm })(BookForm)
