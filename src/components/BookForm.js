dbd22ba3feddc0ac92acf18400b8ef7daba793f1
Dakota <dakotakcoppage@me.com>
2021-08-01 10:39:33 -0600

    created book form div

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -3,3 +3,21 @@ import { Form } from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { updateBookForm } from '../actions/bookEntryForm'
 import { Button } from 'react-bootstrap'
+
+const BookForm = () => {
+    return (
+        <Form>
+            <Form.Group>
+                <Form.Label>Title</Form.Label>
+                <Form.Control></Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label></Form.Label>
+                <Form.Control></Form.Control>
+            </Form.Group>
+            <Button type="submit" className="btn btn-dark btn-lg btn-block" value="Add Book">Add Book</Button>
+        </Form>
+    )
+}
+
+export default BookForm
