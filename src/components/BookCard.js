140b71aa2b7eb238d354a069d65e562f51db69c8
Dakota <dakotakcoppage@me.com>
2021-08-01 16:22:40 -0600

    added category and changed some styling

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -4,7 +4,7 @@ import { Card } from 'react-bootstrap'
 
 const BookCard = ({ book }) => {
     return (
-        <Card style={{ width: '18rem', boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)" }}>
+        <Card style={{ width: '8rem', boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)" }}>
             <Card.Body>
                 <Card.Img variant="top" src={book.attributes.image_url}/>
                 <Card.Title>{book.attributes.title}</Card.Title>
