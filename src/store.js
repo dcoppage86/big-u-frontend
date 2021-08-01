7c9535fe0b53099b837e350bcceb1b4f2872f0ea
Dakota <dakotakcoppage@me.com>
2021-08-01 09:07:50 -0600

    passed in bookEntryForm reducer

--- src/store.js
+++ src/store.js
@@ -7,6 +7,7 @@ import userEntries from './reducers/userEntries.js'
 import signupForm from './reducers/signupForm'
 import allBooks from './reducers/books.js'
 import userEntryForm from './reducers/userEntryForm.js'
+import bookEntryForm from './reducers/bookEntryForm.js'
 
 const reducer = combineReducers({
   users: usersReducers,
@@ -15,7 +16,8 @@ const reducer = combineReducers({
   userEntries,
   signupForm,
   allBooks,
-  userEntryForm
+  userEntryForm,
+  bookEntryForm
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
