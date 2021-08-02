136eeb313f8e65f14250c056059ae7ab42ef1e2e
Dakota <dakotakcoppage@me.com>
2021-08-02 08:18:35 -0600

    add books and delete books

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,7 +1,13 @@
-const allBooks = (state = [], action) => {
+const initialState = []
+
+const allBooks = (state = initialState, action) => {
     switch (action.type) {
         case "SET_BOOKS":
             return action.books
+        case "ADD_BOOK":
+            return state.concat(action.book)
+        case "DELETE_BOOK":
+            return state.filter(book => book.id === action.bookId ? false : true)
         default:
             return state
     }
