f08e057f8e35b33c58ce93cd27b2dfa7ffdbc57a
Dakota <dakotakcoppage@me.com>
2021-08-02 09:53:41 -0600

    added delete button and handleClick

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { deleteBook } from '../actions/books'
 
 
-const BookCard = ({ book }) => {
+const BookCard = ({ book, deleteBook }) => {
+    const bookId = book ? book.id : null
+
+    const handleClick = event => {
+        console.log("this deletes the book")
+        event.preventDefault()
+        deleteBook(bookId)
+    }
     return (
         <Card style={{ width: '8rem', boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)" }}>
             <Card.Body>
@@ -10,6 +20,7 @@ const BookCard = ({ book }) => {
                 <Card.Title>{book.attributes.book_title}</Card.Title>
                 <Card.Text>{book.attributes.book_author}</Card.Text>
                 <Card.Link href={book.attributes.book_url}>Find Here</Card.Link>
+                <Button type="submit" className="btn btn-dark btn-sm btn-block" value="Delete" onClick={ handleClick }>Remove</Button>
             </Card.Body>
         </Card>
 
@@ -17,4 +28,4 @@ const BookCard = ({ book }) => {
     )
 }
 
-export default BookCard
+export default connect(null, {deleteBook})(BookCard)
