a5434711b8379e2780e7e89ce672fa6d2709481f
Dakota <dakotakcoppage@me.com>
2021-08-02 08:22:09 -0600

    added fetch for adding book to library here instead

--- src/actions/books.js
+++ src/actions/books.js
@@ -1,7 +1,16 @@
+import { updateBookForm, resetBookForm } from "./bookEntryForm"
+
 export const setBooks = books => {
     return {
         type: "SET_BOOKS",
         books
+    }
+}
+
+export const addBook = book => {
+    return {
+        type: "ADD_BOOK",
+        book
     }
 }
 
@@ -21,6 +30,39 @@ export const getBooks = () => {
             } else {
                 console.log(response.data)
                 dispatch(setBooks(response.data))
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+export const newBook = (bookData) => {
+    return dispatch => {
+        const sendBookData = {
+            book_title: bookData.book_title,
+            book_author: bookData.book_author,
+            image_url: bookData.image_url,
+            book_url: bookData.book_url,
+            user_id: bookData.userId
+        }
+        return fetch("http://localhost:3001/api/v1/books", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(sendBookData)
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+            if (response.error) {
+                alert(response.error)
+            } else {
+                dispatch(addBook(response.data))
+                setBooks(response.data)
+                dispatch(updateBookForm(response))
+                dispatch(resetBookForm())
             }
         })
         .catch(console.log)
