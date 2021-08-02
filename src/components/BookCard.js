753c0a381df6b3d074cdbfd1c84a2881ded2d2b4
Dakota <dakotakcoppage@me.com>
2021-08-02 08:19:22 -0600

    updated attributes

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -7,8 +7,8 @@ const BookCard = ({ book }) => {
         <Card style={{ width: '8rem', boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)" }}>
             <Card.Body>
                 <Card.Img variant="top" src={book.attributes.image_url}/>
-                <Card.Title>{book.attributes.title}</Card.Title>
-                <Card.Text>{book.attributes.author}</Card.Text>
+                <Card.Title>{book.attributes.book_title}</Card.Title>
+                <Card.Text>{book.attributes.book_author}</Card.Text>
                 <Card.Link href={book.attributes.book_url}>Find Here</Card.Link>
             </Card.Body>
         </Card>
