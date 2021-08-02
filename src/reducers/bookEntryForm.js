9fb6e4ced8979256a5e99426aab4e451e6459425
Dakota <dakotakcoppage@me.com>
2021-08-02 06:52:45 -0600

    fixed formData to be bookFormData to grab the name and value of the book form

--- src/reducers/bookEntryForm.js
+++ src/reducers/bookEntryForm.js
@@ -1,17 +1,16 @@
 const initialState = {
-    title: "",
-    author: "",
+    book_title: "",
+    book_author: "",
     image_url: "",
-    book_url: "",
-    category: ""
+    book_url: ""
 }
 
 const bookEntryForm = (state=initialState, action) => {
     switch (action.type) {
-        case "UPDATE_ENTRY_FORM":
+        case "UPDATE_BOOK_FORM":
             const returnInfo = {
                 ...state,
-                [action.formData.name]: action.formData.value
+                [action.bookFormData.name]: action.bookFormData.value
             }
             return returnInfo
         case "RESET_ENTRY_FORM":
